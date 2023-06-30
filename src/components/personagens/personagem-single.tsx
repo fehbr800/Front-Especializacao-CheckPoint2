da0d3d8dedd793d7bdae4b380cff71d03805b33f
DylanTokumoto <heidy.tokumoto@gmail.com>
2023-06-30 11:21:05 -0300

    correção erro de zoom da página details

--- src/components/personagens/personagem-single.tsx
+++ src/components/personagens/personagem-single.tsx
@@ -17,7 +17,7 @@ const PersonagemSingle = ({
 }: ICardPersonagemIndividualProps) => {
   return (
     <>
-      <div className="flex items-center justify-around max-h-screen mb-7">
+      <div className="flex items-center justify-around h-full mb-7 overflow-y-hidden">
         <div className="max-w-md rounded-lg shadow-md overflow-hidden">
           <div className="p-4 text-slate-50">
             <h1 className="text-2xl font-bold mb-2">{name}</h1>
