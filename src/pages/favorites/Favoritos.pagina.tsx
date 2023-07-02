34a575ee037897c2de7e383dcaf5c529fb12266f
Matheus Emanoel <matheusemanoel1407@gmail.com>
2023-07-02 10:09:34 -0400

    final final

--- src/pages/favorites/Favoritos.pagina.tsx
+++ src/pages/favorites/Favoritos.pagina.tsx
@@ -2,14 +2,17 @@ import GradePersonagemFavoritos from "../../components/personagens/garde-persona
 
 const PaginaFavoritos = () => {
   return (
-    <div className="h-screen overflow-hidden flex flex-col items-center">
+    <div className="min-h-screen overflow-hidden flex flex-col items-center">
       <div className="actions">
-        <h3>Personagens Favoritos</h3>
+        <h3 className=" text-slate-50 font-custom mt-3 text-2xl">Personagens Favoritos</h3>
         <button className="danger">Test Button</button>
       </div>
-      <GradePersonagemFavoritos />
+      <div className="flex flex-col items-center mt-8">
+        <GradePersonagemFavoritos />
+      </div>
     </div>
   );
 };
+
 
 export default PaginaFavoritos;
