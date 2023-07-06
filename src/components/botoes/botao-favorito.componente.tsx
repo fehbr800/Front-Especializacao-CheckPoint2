9afbf391d00c5391f884e3827c8958edeb1e9835
Matheus Emanoel <matheusemanoel1407@gmail.com>
2023-07-06 09:56:38 -0400

    botão

--- src/components/botoes/botao-favorito.componente.tsx
+++ src/components/botoes/botao-favorito.componente.tsx
@@ -13,10 +13,11 @@ const BotaoFavorito = ({ isFavorito, onClick }: BotaoFavoritoProps) => {
 <button
   type="button"
   onClick={() => onClick(!isFavorito)}
+  className="hover:opacity-75"
   
 >
   <div className="botao-favorito">
-    <img src={src} alt="favorito" className=" w-12 h-12 sm:w-16 sm:h-16 md:w-18 md:h-18 lg:w-20 lg:h-20" />
+    <img src={src} alt="favorito" className="w-12 h-12 sm:w-16 sm:h-16 md:w-18 md:h-18 lg:w-20 lg:h-20" />
   </div>
 </button>
 
