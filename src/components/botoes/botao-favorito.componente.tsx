12849ac8059398952ac6d83c402e916f78d61974
Matheus Emanoel <matheusemanoel1407@gmail.com>
2023-07-06 09:12:33 -0400

    button-new

--- src/components/botoes/botao-favorito.componente.tsx
+++ src/components/botoes/botao-favorito.componente.tsx
@@ -10,15 +10,16 @@ const BotaoFavorito = ({ isFavorito, onClick }: BotaoFavoritoProps) => {
   const src = isFavorito ? filled : notfilled;
 
   return (
-    <button
-      type="button"
-      onClick={() => onClick(!isFavorito)}
-      className=" text-white font-bold py-2 px-4 rounded-full"
-    >
-      <div className="botao-favorito">
-        <img src={src} alt="favorito" className="w-8 h-8" />
-      </div>
-    </button>
+<button
+  type="button"
+  onClick={() => onClick(!isFavorito)}
+  
+>
+  <div className="botao-favorito">
+    <img src={src} alt="favorito" className=" w-12 h-12 sm:w-16 sm:h-16 md:w-18 md:h-18 lg:w-20 lg:h-20" />
+  </div>
+</button>
+
   );
   
   
